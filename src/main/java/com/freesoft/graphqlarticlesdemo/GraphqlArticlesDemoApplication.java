80409890c246d6d5e66f0afe929ee8d3a26968dc
dinabogdan <bogdan.dina03@gmail.com>
2018-07-09 20:54:24 +0300

    first commit

---
+++ src/main/java/com/freesoft/graphqlarticlesdemo/GraphqlArticlesDemoApplication.java
@@ -0,0 +1,12 @@
+package com.freesoft.graphqlarticlesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlArticlesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlArticlesDemoApplication.class, args);
+	}
+}
